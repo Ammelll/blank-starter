0b6296ec9984bcebf8e93f5bb76c8395712afbb3
Ammel <77129284+Ammelll@users.noreply.github.com>
2025-08-15 18:41:29 +0000

    start of CRUD library

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-document.getElementById("button").addEventListener("click",myFunc);
+document.getElementById("push").addEventListener("click",push);
+document.getElementById("pull").addEventListener("click",pull);
 
-function myFunc(){
-    document.getElementById("output").innerHTML = "hi friend!";
+// function push(){
+//     let value = document.getElementById("text").value
+//     let object = {t: value}
+//     localStorage.setItem("textField",JSON.stringify(object))
+// }
+
+// function pull(){
+//     p = JSON.parse(localStorage.getItem("textField")).t;
+//     document.getElementById("outputtext").innerHTML = p;
+//     console.log(p)
+// }
+function addBook(){
+    title = document.getElementById("titleFieldAdd").innerHTML
+    author = document.getElementById("authorFieldAdd").innerHTML
+    pages = document.getElementById("pagesFieldAdd").innerHTML
+    localStorage.setItem(title,JSON.stringify(
+        {
+            title:title,
+            author:author,
+            pages:pages,
+        }
+    ))
 }
+function viewBook(title){
+    book = JSON.parse(localStorage.getItem(title))
+    displayBook(book)
+}
+function checkoutBook(title){
+    viewBook(title)
+    localStorage.removeItem(title)
+}
+function editBook(title){
+    document.getElementById("authorField").hidden = false;
+    document.getElementById("pagesField").hidden = false;
+    document.getElementById("submitEdit").hidden = false;
+}
+function displayBook(book){
+    document.getElementById("title").innerHTML = book.title;
+    document.getElementById("author").innerHTML = book.author;
+    document.getElementById("pages").innerHTML = book.pages
+}
+
 
