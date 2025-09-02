b489873fae029a7b10d5a82cc2e54c036b6228f6
Ammel <77129284+Ammelll@users.noreply.github.com>
2025-09-02 18:42:16 +0000

    updated with crud

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -1,13 +1,22 @@
 'use client'
 
 import { useSearchParams } from "next/navigation"
+import { useState } from "react";
 
 export default function BookDisplay(){
     const book = JSON.parse(localStorage.getItem(useSearchParams()?.get('id') ?? '') ?? '{}');
+    let button = <button onClick={addBook}>Add Book</button>;
+    if(!book){button = <div></div>}
+    console.log(book)
+
     return (<div>
-    <div>Title: {book?.title}</div>
-    <div>Author: {book?.author}</div>
-    <p>Description: {book?.description}</p>
+    <div>Title: {book.title || <input></input>}</div>
+    <div>Author: {book.author || <input></input>}</div>
+    <p>Description: {book.description || <input></input>}</p>
+    {button}
     </div>
     )
     }
+function addBook(){
+
+}
--- ui/table.tsx
+++ ui/table.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 class Book {
     id!: number;
     title!: string;
@@ -8,6 +9,7 @@ class Book {
     description!: string;
 }
 export default function BookTable(){
+    const router = useRouter();
     const books = Object.values({...localStorage}).map((book: string) => (JSON.parse(book)));
     let params = new URLSearchParams(useSearchParams());
     return (
@@ -18,6 +20,12 @@ export default function BookTable(){
           <Link href={`/detail?${params}`}>Title: {book.title}</Link> 
         </p>
     })}
+    <button type="button" onClick={() => {
+      params.set('id',(localStorage.length+1).toString());
+      router.replace(`/detail?${params}`)
+    }}>
+      Add Book
+    </button>
     </div>
     );
 }
