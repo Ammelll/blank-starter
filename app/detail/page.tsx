a662b44b5bc68ae178910cd622102d7dbf206d48
Amelll <ammelsallow@gmail.com>
2025-09-02 23:12:00 -0700

    Fixed React Project

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -2,21 +2,36 @@
 
 import { useSearchParams } from "next/navigation"
 import { useState } from "react";
-
+import Book from "@/ui/table";
 export default function BookDisplay(){
-    const book = JSON.parse(localStorage.getItem(useSearchParams()?.get('id') ?? '') ?? '{}');
-    let button = <button onClick={addBook}>Add Book</button>;
-    if(!book){button = <div></div>}
-    console.log(book)
-
+    let id = useSearchParams()?.get('id') ?? '';
+    let update = useSearchParams()?.get('update') ?? '';
+    const book = JSON.parse(localStorage.getItem(id) ?? '{}');
+    let button = <button onClick={() => addBook(id)}>{update ? "update" : "add"}</button>   
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [description, setDescription] = useState("");
+    if(Object.keys(book).length != 0 && !update){button = <div></div>}
     return (<div>
-    <div>Title: {book.title || <input></input>}</div>
-    <div>Author: {book.author || <input></input>}</div>
-    <p>Description: {book.description || <input></input>}</p>
+   <div>
+  Title: {(book.title && !update) ? book.title : <input onChange={(e) => setTitle(e.target.value)} />}
+</div>
+<div>
+  Author: {(book.author && !update) ? book.author : <input onChange={(e) => setAuthor(e.target.value)} />}
+</div>
+<p>
+  Description: {(book.description && !update) ? book.description : <input onChange={(e) => setDescription(e.target.value)} />}
+</p>
     {button}
     </div>
     )
-    }
-function addBook(){
-
+    function addBook(id:string){
+        let book = {
+            id: Number(id),
+            title: title,
+            author: author,
+            description: description,
+        }
+        localStorage.setItem(id,JSON.stringify(book));
 }
+    }
--- ui/table.tsx
+++ ui/table.tsx
@@ -17,15 +17,18 @@ export default function BookTable(){
       {books?.map((book: Book) => {
         params.set('id',book.id.toString());
         return <p key={book.id}>
-          <Link href={`/detail?${params}`}>Title: {book.title}</Link> 
+          <Link href={`/detail?${params}`}>Title: {book.title}</Link>  <Link href={`/detail?${params}&update=true`}>UPDATE</Link>    <button onClick={()=> deleteBook(book.id.toString())}>DELETE</button>
         </p>
     })}
     <button type="button" onClick={() => {
-      params.set('id',(localStorage.length+1).toString());
+      params.set('id',((Math.max(...Object.keys(localStorage).map(s => Number(s)))+1)).toString());
       router.replace(`/detail?${params}`)
     }}>
       Add Book
     </button>
     </div>
     );
+}
+function deleteBook(id:string){
+  localStorage.removeItem(id);
 }
